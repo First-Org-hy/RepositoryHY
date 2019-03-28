47f077b7b399fd5beadff3b6fb5e86f1b5277fe4
KitRunnerHouYao <houyao0921@foxmail.com>
2019-03-29 00:38:52 +0800

    create table ing...

--- src/sql/user.sql
+++ src/sql/user.sql
@@ -9,82 +9,86 @@ CREATE TABLE housecd.t_user(
 ) ENGINE=INNODB AUTO_INCREMENT=1000 DEFAULT CHARSET=utf8;
 select * from housecd.t_user;
 
-
-
+/*用户表*/
 CREATE TABLE housecd.app_info(
+  userId INT NOT NULL PRIMARY KEY AUTO_INCREMENT, /*用户id */
+  appName VARCHAR(255),			/*微信名 */
+  appId VARCHAR(255) NOT NULL , /*微信id */
+  appSecret VARCHAR(255) NOT NULL ,  /*微信秘钥 */
+  userName							/*用户姓名 */
+  userIdCard						/*用户身份证号 */
+  userPhone VARCHAR(255),			/*用户手机号 */
+  userType VARCHAR(255) NOT NULL,  /*系统管理员，编辑，楼盘商务，经济公司，经纪人，普通用户*/    
+  userComment          /*客户备注*/     
+  recommendHousesId		/*客户推荐楼盘 */
+  recommendHouseTypeId	/*客户推荐户型 */  
+  companyName			/*公司名称 */
+  parentId 				/*公司id */
+  updateTime TIMESTAMP	/*更新时间 */
+  crtTime TIMESTAMP ,	/*创建时间 */
+  dataState				/*数据状态 */
+) ENGINE=INNODB AUTO_INCREMENT=1000000 DEFAULT CHARSET=utf8;
+
+/*推荐表*/
+create table housecd.recommend(
   id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  appName VARCHAR(255),
-  appId VARCHAR(255) NOT NULL ,
-  appSecret VARCHAR(255) NOT NULL ,
-  appStatus VARCHAR(255) NOT NULL,  /*系统管理员，编辑，楼盘商务，经济公司，经纪人，普通用户*/
-  phone VARCHAR(255),
-  crtTime TIMESTAMP ,
-  rcntlyTime TIMESTAMP
+  userId       /*经济人id */
+  userName		
+  guestId	/*客户id */
+  guestName
+  guestPhone 
+  dealState		/*成交状态 */
+  dead_line		/*到期时间 */
+  crtTime timestamp,
+  dataState
 ) ENGINE=INNODB AUTO_INCREMENT=1000 DEFAULT CHARSET=utf8;
-select * from housecd.app_info
 
-
-CREATE table housecd.agent(
-  id int not null primary key auto_increment,
-  agent_id varchar(255) comment '',
-  agent_name varchar(255) comment '',
-  agent_id_card varchar(255) comment '',
-  recommend_num int(10) comment '',
-  trade_volume int(10) comment '',
-  already_money varchar(255) comment '',
-  stay_money varchar(255) comment '',
-  status char(1) comment '',
-  crtTime timestamp comment ''
-)
-
-CREATE table housecd.company(
-  id int not null primary key auto_increment,
-  company_id varchar(255) comment '',
-  company_name varchar(255) comment '',
-  duty_id varchar(255) comment '',
-  id_card varchar(255) comment '',
-  manager_num varchar(255) comment '',
-  recommend_num int(10) comment '',
-  trade_volume int(10) comment '',
-  already_money varchar(255) comment '',
-  stay_money varchar(255) comment '',
-  status char(1) comment '',
-  crtTime timestamp comment ''
-)
-
-create table housecd.recommend(
-  agent_name
-
-  recommend_name
-  phone 
-  deal
-  dead_line
-  crtTime timestamp comment '',
-
-)
-
-create table housecd.recommend(
-  agent_name
-
-  recommend_name
-  phone 
-  deal
-  dead_line
-  crtTime timestamp comment '',
-
-)
-
+/*佣金表*/
 create table housecd.brokerage(
-    agent_name
-
-  recommend_name
-  phone 
-  deal
-  dead_line
-  brokerage
+id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  userId	/*经济人id */
+  userName
+  guestId   /*客户id */
+  guestName
+  guestPhone 
+  brokerage   	/*佣金 */
+  brokerageState  /*佣金状态 */
   crtTime timestamp comment '',
+  dataState
+)ENGINE=INNODB AUTO_INCREMENT=1000 DEFAULT CHARSET=utf8;
 
+/*佣金参数表*/
+create table housecd.brokerage_param(
+  id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  housesId    /*楼盘id */
+  userParentId   /*经纪公司id */
+  userId		/*经纪人id */
+  address		/*所属区域 */
+  brokerage		/*佣金 */
+  brokerageType /*佣金类型：1.固定，2.比例 */
+  crtTime timestamp comment '',
+  dataState
+) ENGINE=INNODB AUTO_INCREMENT=1000 DEFAULT CHARSET=utf8;
 
-)
-
+/*销售表*/
+create table housecd.sell(
+  id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  housesBusinessId  /*楼盘商务id */
+  userId		/*经纪人id */
+  userParentId  /*经纪公司id */
+  pictureAddress /*图片地址 */  
+  guestId		/*客户id */
+  guestName
+  guestIdCard
+  guestAddress  
+  housesId		/*楼盘id */
+  houseTypeId   /*户型id */ 
+  houseNum		/*房间号 */	
+  price			/*单价 */
+  totalPrice    /*总价 */   
+  brokerage		/*佣金 */
+  brokerageState  /*佣金状态 */
+  crtTime timestamp,
+  dataState
+) ENGINE=INNODB AUTO_INCREMENT=1000 DEFAULT CHARSET=utf8;
 
